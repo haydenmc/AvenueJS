dbcd378a9cf72507954de02613802d2b7cb563f5
Hayden <hayden@outlook.com>
2014-09-06 12:28:23 -0400

    SignalR initial hub with connect and disconnect

--- AvenueJS/App_Start/BundleConfig.cs
+++ AvenueJS/App_Start/BundleConfig.cs
@@ -9,11 +9,12 @@ namespace AvenueJS
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/js").Include(
-						"~/Scripts/js/createjs-2013.12.12.min.js"));
+						"~/Scripts/js/createjs-2013.12.12.min.js",
+						"~/Scripts/jquery-1.6.4.min.js",
+						"~/Scripts/jquery.signalR-2.1.1.min.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/ts").Include(
 						"~/Scripts/ts/*.js",
-
 						"~/Scripts/ts/entities/terrain.js",
 						"~/Scripts/ts/entities/projectile.js",
 						"~/Scripts/ts/entities/character.js",
---
+++ AvenueJS/Scripts/ts/debug.ts
@@ -0,0 +1,5 @@
+﻿interface Window {
+	debug;
+}
+
+window.debug = true; 
