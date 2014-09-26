d906be64cd684bf7951291b623328e18d5bbb17c
Darren Cheng <zades@outlook.com>
2014-09-26 17:37:14 -0400

    Early proof of concept for movement and rotation to mouse

--- AvenueJS/Scripts/ts/main.ts
+++ AvenueJS/Scripts/ts/main.ts
@@ -1,37 +1,31 @@
-﻿class Main {
+﻿class Main
+{
 	static instance: Main;
 	//private canvas: HTMLCanvasElement;
 	public hub: Hub;
-	private game: Phaser.Game;
+	private tinyGunsGame: TinyGunsGame;
 
-	constructor() {
+	constructor()
+	{
 		Main.instance = this;
 		//this.canvas = document.getElementsByTagName("canvas")[0];
 		this.init();
 	}
 
-	public init() {
+	public init()
+	{
 		// Initialize the canvas size.
-		this.game = new Phaser.Game(1024, 768, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+		this.tinyGunsGame = new TinyGunsGame();
 		//this.resizeCanvas();
 	}
-	
-	public preload(): void {
-		this.game.load.image('logo', '/Content/images/vs.png');
-	}
-
-	public create(): void {
-		var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-		logo.anchor.setTo(0.5, 0.5);
-	}
-
 	//public resizeCanvas() {
 	//	this.canvas.width = window.innerWidth;
 	//	this.canvas.height = window.innerHeight;
 	//}
 }
 
-window.onload = () => {
+window.onload = () =>
+{
 	(new Main());
 }
 
