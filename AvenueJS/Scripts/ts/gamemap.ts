ac858ab251e338fa2b0d40ecef1d26a246dffba8
Darren Cheng <zades@outlook.com>
2014-09-29 13:40:09 -0400

    Added a map manager and cleaned up some files

---
+++ AvenueJS/Scripts/ts/gamemap.ts
@@ -0,0 +1,23 @@
+﻿class Gamemap
+{
+	public game: Phaser.Game;
+	public map: Phaser.Tilemap;
+	public mapLayer: Phaser.TilemapLayer;
+	public tilemap: string;
+	public tileImage: string;
+	public mapSize: number;
+
+	public constructor(tilemap: string, tileImage: string, size: number, game: Phaser.Game)
+	{
+		console.log("gamemap created");
+		this.game = game;
+		this.mapSize = size;
+		this.tilemap = tilemap;
+		this.tileImage = tileImage;
+
+		this.map = this.game.add.tilemap(tilemap, this.mapSize, this.mapSize);
+		this.map.addTilesetImage(tileImage);
+		this.mapLayer = this.map.createLayer(0);
+		this.mapLayer.resizeWorld();
+	}
+}
---
+++ AvenueJS/Scripts/ts/map_manager.ts
@@ -0,0 +1,28 @@
+﻿class MapManager
+{
+	//List of map names
+	public static ISLAND_MAP: string = "island"
+	public game: Phaser.Game;
+	public maps: { [mapName: string]: Gamemap; } = {};
+	public currentMap: Gamemap;
+
+	public constructor(game: Phaser.Game)
+	{
+		this.game = game;
+
+		//Beging adding maps
+		//adding island
+		this.maps[MapManager.ISLAND_MAP] = new Gamemap(TinyGunsGame.MAP_PLACEHOLDER, TinyGunsGame.TILES_PLACEHOLDER, 32, this.game);
+		
+		console.log("map manager created");
+
+		//set default map
+		this.currentMap = this.maps[MapManager.ISLAND_MAP];
+		console.log(this.maps[MapManager.ISLAND_MAP]);
+	}
+
+	public switchToMap(mapName: string)
+	{
+		//TODO
+	}
+}
